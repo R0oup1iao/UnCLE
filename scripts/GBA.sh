668b06a53ffbeb9d88631c683e33707504feb7a7
r0oup1iao <yanhaoyang@buaa.edu.cn>
2025-11-26 18:21:19 +0800

    修改了一个单一训练流程的模型，并改进了因果提取方法为梯度，在SD和GBA上实验，感觉还需要提升模型性能

--- scripts/GBA.sh
+++ scripts/GBA.sh
@@ -1,9 +1,10 @@
 accelerate launch src/run.py \
-    --dataset SD \
+    --dataset GBA \
     --data_path data/real \
-    --N 716 \
-    --hierarchy 32 8 \
+    --N 2352 \
+    --hierarchy 512 256 32 8 \
     --window_size 12 \
     --lambda_ent 1e-4 \
     --lambda_bal 2.0 \
-    --tau_decay 0.005
+    --tau_decay 0.005 \
+    --batch_size 16
